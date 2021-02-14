ea0f47a675802a6178e6cd8c57f5213426730589
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-14 15:52:55 -0700

    Update Src.py

--- Src.py
+++ Src.py
@@ -12,8 +12,8 @@ if len(arr) > 1:
 else:
     min_diff = 0
 
-for n1, n2 in itertools.combinations(arr, 2): # Get the combinations of numbers
-    diff = abs(n1-n2) # Find the absolute difference of each combination
+for name_given_1, name_given_2 in itertools.combinations(arr, 2): # Get the combinations of numbers
+    diff = abs(nanem_given_1-name_given_2) # Find the absolute difference of each combination
     if min_diff > diff:
         min_diff = diff # Replace incase a least differnce found
 
